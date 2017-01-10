ad5b1d017af941998ef2b0e3ffa73ebedf993865
Wally Jones <jacobwaltjones@gmail.com>
2017-01-10 12:03:51 -0600

    Drill Commit

---
+++ Drill23a/Drill23a/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+namespace Overloading
+{
+    public class Vehicle
+    {
+        public void attributes(int id)
+        { }
+        public void attributes(string name)
+        { }
+    }
+}
+
+namespace Overriding
+{
+    public class Parent
+    {
+        public virtual void Genetics()
+        { }
+    }
+    public class Child : Parent
+    {
+        public override void Genetics()
+        { }
+    }
+}
+
+namespace Derived
+{
+    public class Shape
+    {
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+        protected int width;
+        protected int height;
+    }
+
+    // Derived class
+    class Rectangle : Shape
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+    }
+}
+
+//sealed class
+namespace Sealed
+{
+    public sealed class Shape
+        //this will not work because of the sealed access modifier here
+        //sealed classes are not inheritable but can inherit
+    {
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+        protected int width;
+        protected int height;
+    }
+    class Rectangle : Shape
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+    }
+}
---
+++ Drill23b/Drill23b/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//interface implementation
+namespace Drill23b
+{
+    public interface IInterfaceTest
+    {
+        double getStuff();
+        void somethingToDo();
+    }
+    public class ImplementationTest : IInterfaceTest
+    {
+        private string testCode;
+        private double testNum;
+        public ImplementationTest()
+        {
+            testCode = " ";
+            testNum = 0.0;
+        }
+        public ImplementationTest(string s, double d)
+        {
+            testCode = s;
+            testNum = d;
+        }
+        public double getStuff()
+        {
+            return testNum;
+        }
+        public void somethingToDo()
+        {
+            Console.WriteLine("Testing string: {0}", testCode);
+            Console.WriteLine("Testing integer: {0}", testNum);
+        }
+    }
+    class TestingInter
+    {
+        static void Test(string[] args)
+        {
+            ImplementationTest t1 = new ImplementationTest("First",100.0);
+            ImplementationTest t2 = new ImplementationTest("Second",20000.0);
+            t1.somethingToDo();
+            t2.somethingToDo();
+            Console.ReadKey();
+        }
+    }
+}
